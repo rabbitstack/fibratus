0ed94de02e6fb7bcb511b7bcd7b5753e6f2a7204
Nedim Šabić <bhdildo@gmail.com>
2016-05-27 10:41:39 +0200

    Simplify with collections.Counter()

--- filaments/top_connections.py
+++ filaments/top_connections.py
@@ -14,32 +14,26 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+mport collections
+
 """
 Shows top TCP / UDP connections
 """
 
-connections = {}
-
+connections = collections.Counter()
 
 def on_init():
     set_filter('Send')
     columns(["Port", "IP", "Count"])
     sort_by('Count')
 
-
 def on_next_kevent(kevent):
-    k = (kevent.params.dport, kevent.params.ip_dst)
-    if k in connections:
-        count = connections[k]
-        connections[k] = count + 1
-    else:
-        connections[k] = 1
+    connections.update((kevent.params.dport, kevent.params.ip_dst))
 
     for t, count in connections.items():
         add_row([t[0], t[1], count])
 
     render_tabular()
-
 
 def on_stop():
     pass
