53d8b76b74b8a4ff594901e0a3e0bd824fe2bd5b
rabbitstack <bhnedo@hotmail.com>
2016-06-04 13:32:51 +0200

    Include the proper method

--- tests/unit/common.py
+++ tests/unit/common.py
@@ -14,7 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from mock import patch
+from unittest.mock import patch
 from fibratus.common import IO
 
 
