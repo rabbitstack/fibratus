07f3f84b75ac3af060f0204032676fe21797f365
rabbitstack <bhnedo@hotmail.com>
2016-06-04 13:01:48 +0200

    Native console output

--- fibratus/common.py
+++ fibratus/common.py
@@ -14,6 +14,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from _ctypes import byref
+from ctypes import c_ulong
+from fibratus.apidefs.sys import write_console_unicode, get_std_handle, STD_OUTPUT_HANDLE, close_handle
+
 NA = '<NA>'
 
 
@@ -49,9 +53,28 @@ class DotD(dict):
 
 class IO():
 
+    _stdout_handle = get_std_handle(STD_OUTPUT_HANDLE)
+    assert _stdout_handle, 'could not acquire the standard output stream handle'
+
     @classmethod
-    def write_console(cls, chars, new_line=True):
+    def write_console(cls, charseq, new_line=True):
+        """Outputs to a Windows console using UNICODE charset.
+
+        Parameters
+        ----------
+
+        charseq: str
+            the sequence of characters to be written
+
+        new_line: bool
+            indicates if the output should be written on the new line
+        """
         if new_line:
-            print(chars)
+            charseq += '\n'
         else:
-            print(chars, end='\r')
+            charseq += '\r'
+        write_console_unicode(cls._stdout_handle, charseq, len(charseq), byref(c_ulong()), None)
+
+    def __del__(self):
+        if self._stdout_handle:
+            close_handle(self._stdout_handle)
