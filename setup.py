c6a1c0e7b121211b03e5d96b7b5dcaf48cf16934
rabbitstack <bhnedo@hotmail.com>
2016-07-16 11:31:42 +0200

    Install kstreamc to site-packages

--- setup.py
+++ setup.py
@@ -2,9 +2,23 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+kstreamc_ext = Extension('kstreamc',
+                         ['kstream/kstreamc.pyx'],
+                         libraries=["tdh", "advapi32", "ole32"])
+
 setup(
-    ext_modules=[Extension("kstreamc",
-                ["kstream/kstreamc.pyx"],
-        libraries=["tdh", "advapi32", "ole32"])],
+    name="kstreamc",
+    version="0.2.0",
+    author="Nedim Sabic (RabbitStack)",
+    author_email="bhnedo@hotmail.com",
+    description="Kernel event stream collector for Fibratus",
+    license="Apache",
+    keywords="windows kernel, tracing, exploration",
+    url="https://github.com/rabbitstack/fibratus",
+    classifiers=[
+        "Topic :: System",
+        "License :: OSI Approved :: Apache License"
+    ],
+    ext_modules=[kstreamc_ext],
     cmdclass={"build_ext": build_ext}
 )
