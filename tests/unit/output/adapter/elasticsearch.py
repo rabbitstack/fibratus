3423de840603a5a8bc3ab4b44aa43fcc59e94c48
rabbitstack <bhnedo@hotmail.com>
2016-10-30 17:30:04 +0100

    elasticsearch output adapter

---
+++ tests/unit/output/adapter/elasticsearch.py
@@ -0,0 +1,61 @@
+# Copyright 2016 by Nedim Sabic (RabbitStack)
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from unittest.mock import patch
+
+import elasticsearch
+import pytest
+
+from fibratus.errors import InvalidPayloadError
+from fibratus.output.adapter.elasticsearch import ElasticsearchAdapter
+
+
+@pytest.fixture(scope='module')
+def elasticsearch_adapter():
+    config = {
+        'hosts': [
+            'localhost:9200',
+            'rabbitstack:9200'
+        ],
+        'index': 'kernelstream',
+        'document': 'threads'
+    }
+    return ElasticsearchAdapter(**config)
+
+
+class TestElasticsearchAdapter(object):
+
+    def test_init(self, elasticsearch_adapter):
+        assert isinstance(elasticsearch_adapter.hosts, list)
+        assert len(elasticsearch_adapter.hosts) > 0
+        assert {'host': 'localhost', 'port': 9200} in elasticsearch_adapter.hosts
+        assert elasticsearch_adapter.index_name == 'kernelstream'
+        assert elasticsearch_adapter.document_type == 'threads'
+
+    def test_emit(self, elasticsearch_adapter):
+        body = {'kevent_type': 'CreateProcess', 'params': {'name': 'smss.exe'}}
+        assert elasticsearch_adapter._elasticsearch is None
+        with patch('elasticsearch.Elasticsearch', spec_set=elasticsearch.Elasticsearch) as es_client_mock:
+            elasticsearch_adapter.emit(body)
+            es_client_mock.assert_called_with([{'host': 'localhost', 'port': 9200},
+                                               {'host': 'rabbitstack', 'port': 9200}])
+            elasticsearch_adapter._elasticsearch.index.assert_called_with('kernelstream', 'threads', body=body)
+
+    @patch('elasticsearch.Elasticsearch', spec_set=elasticsearch.Elasticsearch)
+    def test_emit_invalid_payload(self, es_client_mock, elasticsearch_adapter):
+        body = ['CrateProcess', 'TerminateProcess']
+        with pytest.raises(InvalidPayloadError) as e:
+            elasticsearch_adapter.emit(body)
+        assert "invalid payload for document. dict expected but <class 'list'> found" == str(e.value)
+        assert es_client_mock.index.assert_not_called()
