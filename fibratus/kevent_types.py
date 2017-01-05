321a3618052ddfc7206606df6c215368f3bd9de9
rabbitstack <bhnedo@hotmail.com>
2017-01-05 22:41:49 +0100

    added curly brackets in kernel event types

--- fibratus/kevent_types.py
+++ fibratus/kevent_types.py
@@ -18,93 +18,93 @@ from fibratus.kevent import KEvents
 
 
 # start process event
-CREATE_PROCESS = ('3d6fa8d0-fe05-11d0-9dda-00c04fd7ba7c', 1)
+CREATE_PROCESS = ('{3d6fa8d0-fe05-11d0-9dda-00c04fd7ba7c}', 1)
 # end process event
-TERMINATE_PROCESS = ('3d6fa8d0-fe05-11d0-9dda-00c04fd7ba7c', 2)
+TERMINATE_PROCESS = ('{3d6fa8d0-fe05-11d0-9dda-00c04fd7ba7c}', 2)
 # enum processes event
-ENUM_PROCESS = ('3d6fa8d0-fe05-11d0-9dda-00c04fd7ba7c', 3)
+ENUM_PROCESS = ('{3d6fa8d0-fe05-11d0-9dda-00c04fd7ba7c}', 3)
 # start thread event
-CREATE_THREAD = ('3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c', 1)
+CREATE_THREAD = ('{3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c}', 1)
 # end thread event
-TERMINATE_THREAD = ('3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c', 2)
+TERMINATE_THREAD = ('{3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c}', 2)
 # enum threads event
-ENUM_THREAD = ('3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c', 3)
+ENUM_THREAD = ('{3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c}', 3)
 
 # create file event
-CREATE_FILE = ('90cbdc39-4a3e-11d1-84f4-0000f80464e3', 64)
+CREATE_FILE = ('{90cbdc39-4a3e-11d1-84f4-0000f80464e3}', 64)
 # delete file event
-DELETE_FILE = ('90cbdc39-4a3e-11d1-84f4-0000f80464e3', 70)
+DELETE_FILE = ('{90cbdc39-4a3e-11d1-84f4-0000f80464e3}', 70)
 # close file event generated when the file object is freed
-CLOSE_FILE = ('90cbdc39-4a3e-11d1-84f4-0000f80464e3', 66)
+CLOSE_FILE = ('{90cbdc39-4a3e-11d1-84f4-0000f80464e3}', 66)
 # read file event
-READ_FILE = ('90cbdc39-4a3e-11d1-84f4-0000f80464e3', 67)
+READ_FILE = ('{90cbdc39-4a3e-11d1-84f4-0000f80464e3}', 67)
 # write file event
-WRITE_FILE = ('90cbdc39-4a3e-11d1-84f4-0000f80464e3', 68)
+WRITE_FILE = ('{90cbdc39-4a3e-11d1-84f4-0000f80464e3}', 68)
 # rename file event
-RENAME_FILE = ('90cbdc39-4a3e-11d1-84f4-0000f80464e3', 71)
+RENAME_FILE = ('{90cbdc39-4a3e-11d1-84f4-0000f80464e3}', 71)
 # enumerate directory event
-ENUM_DIRECTORY = ('90cbdc39-4a3e-11d1-84f4-0000f80464e3', 72)
+ENUM_DIRECTORY = ('{90cbdc39-4a3e-11d1-84f4-0000f80464e3}', 72)
 
 # disk read event
-DISK_IO_READ = ('3d6fa8d4-fe05-11d0-9dda-00c04fd7ba7c', 10)
+DISK_IO_READ = ('{3d6fa8d4-fe05-11d0-9dda-00c04fd7ba7c}', 10)
 # disk write event
-DISK_IO_WRITE = ('3d6fa8d4-fe05-11d0-9dda-00c04fd7ba7c', 11)
+DISK_IO_WRITE = ('{3d6fa8d4-fe05-11d0-9dda-00c04fd7ba7c}', 11)
 
 # create registry key event
-REG_CREATE_KEY = ('ae53722e-c863-11d2-8659-00c04fa321a1', 10)
+REG_CREATE_KEY = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 10)
 # create registry key event
-REG_DELETE_KEY = ('ae53722e-c863-11d2-8659-00c04fa321a1', 12)
+REG_DELETE_KEY = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 12)
 # delete registry value event
-REG_DELETE_VALUE = ('ae53722e-c863-11d2-8659-00c04fa321a1', 15)
+REG_DELETE_VALUE = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 15)
 # registry open key
-REG_OPEN_KEY = ('ae53722e-c863-11d2-8659-00c04fa321a1', 11)
+REG_OPEN_KEY = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 11)
 # registry set value key event
-REG_SET_VALUE = ('ae53722e-c863-11d2-8659-00c04fa321a1', 14)
+REG_SET_VALUE = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 14)
 # registry query value key event
-REG_QUERY_VALUE = ('ae53722e-c863-11d2-8659-00c04fa321a1', 16)
+REG_QUERY_VALUE = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 16)
 # registry query value key event
-REG_QUERY_KEY = ('ae53722e-c863-11d2-8659-00c04fa321a1', 13)
+REG_QUERY_KEY = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 13)
 # create the key control block
-REG_CREATE_KCB = ('ae53722e-c863-11d2-8659-00c04fa321a1', 22)
+REG_CREATE_KCB = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 22)
 # delete the key control block
-REG_DELETE_KCB = ('ae53722e-c863-11d2-8659-00c04fa321a1', 23)
+REG_DELETE_KCB = ('{ae53722e-c863-11d2-8659-00c04fa321a1}', 23)
 
 # image load event generated when a DLL or executable file is loaded
-LOAD_IMAGE = ('2cb15d1d-5fc1-11d2-abe1-00a0c911f518', 10)
+LOAD_IMAGE = ('{2cb15d1d-5fc1-11d2-abe1-00a0c911f518}', 10)
 # generated when a DLL or executable file is unloaded
-UNLOAD_IMAGE = ('2cb15d1d-5fc1-11d2-abe1-00a0c911f518', 2)
+UNLOAD_IMAGE = ('{2cb15d1d-5fc1-11d2-abe1-00a0c911f518}', 2)
 # enumerates all loaded images
-ENUM_IMAGE = ('2cb15d1d-5fc1-11d2-abe1-00a0c911f518', 3)
+ENUM_IMAGE = ('{2cb15d1d-5fc1-11d2-abe1-00a0c911f518}', 3)
 
 # virtual memory allocation event
-VIRTUAL_ALLOC = ('3d6fa8d3-fe05-11d0-9dda-00c04fd7ba7c', 98)
+VIRTUAL_ALLOC = ('{3d6fa8d3-fe05-11d0-9dda-00c04fd7ba7c}', 98)
 # virtual memory free event
-VIRTUAL_FREE = ('3d6fa8d3-fe05-11d0-9dda-00c04fd7ba7c', 99)
+VIRTUAL_FREE = ('{3d6fa8d3-fe05-11d0-9dda-00c04fd7ba7c}', 99)
 
 # system call enter event
-SYSCALL_ENTER = ('ce1dbfb4-137e-4da6-87b0-3f59aa102cbc', 51)
+SYSCALL_ENTER = ('{ce1dbfb4-137e-4da6-87b0-3f59aa102cbc}', 51)
 # system call exit event
-SYSCALL_EXIT = ('ce1dbfb4-137e-4da6-87b0-3f59aa102cbc', 52)
+SYSCALL_EXIT = ('{ce1dbfb4-137e-4da6-87b0-3f59aa102cbc}', 52)
 
 # context switch event
-CONTEXT_SWITCH = ('3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c', 36)
+CONTEXT_SWITCH = ('{3d6fa8d1-fe05-11d0-9dda-00c04fd7ba7c}', 36)
 
 # starts an incoming connection attempt on socket
-ACCEPT_SOCKET_TCPV4 = ('9a280ac0-c8e0-11d1-84e2-00c04fb998a2', 15)
-ACCEPT_SOCKET_TCPV6 = ('9a280ac0-c8e0-11d1-84e2-00c04fb998a2', 31)
+ACCEPT_SOCKET_TCPV4 = ('{9a280ac0-c8e0-11d1-84e2-00c04fb998a2}', 15)
+ACCEPT_SOCKET_TCPV6 = ('{9a280ac0-c8e0-11d1-84e2-00c04fb998a2}', 31)
 
 # sends data on a connected socket
-SEND_SOCKET_TCPV4 = ('9a280ac0-c8e0-11d1-84e2-00c04fb998a2', 10)
-SEND_SOCKET_UDPV4 = ('bf3a50c5-a9c9-4988-a005-2df0b7c80f80', 10)
+SEND_SOCKET_TCPV4 = ('{9a280ac0-c8e0-11d1-84e2-00c04fb998a2}', 10)
+SEND_SOCKET_UDPV4 = ('{bf3a50c5-a9c9-4988-a005-2df0b7c80f80}', 10)
 # establishes a connection to a specified socket
-CONNECT_SOCKET_TCPV4 = ('9a280ac0-c8e0-11d1-84e2-00c04fb998a2', 12)
+CONNECT_SOCKET_TCPV4 = ('{9a280ac0-c8e0-11d1-84e2-00c04fb998a2}', 12)
 # disconnect event
-DISCONNECT_SOCKET_TCPV4 = ('9a280ac0-c8e0-11d1-84e2-00c04fb998a2', 13)
+DISCONNECT_SOCKET_TCPV4 = ('{9a280ac0-c8e0-11d1-84e2-00c04fb998a2}', 13)
 # reconnect attempt event
-RECONNECT_SOCKET_TCPV4 = ('9a280ac0-c8e0-11d1-84e2-00c04fb998a2', 16)
+RECONNECT_SOCKET_TCPV4 = ('{9a280ac0-c8e0-11d1-84e2-00c04fb998a2}', 16)
 # receives data from a connected socket
-RECV_SOCKET_TCPV4 = ('9a280ac0-c8e0-11d1-84e2-00c04fb998a2', 11)
-RECV_SOCKET_UDPV4 = ('bf3a50c5-a9c9-4988-a005-2df0b7c80f80', 11)
+RECV_SOCKET_TCPV4 = ('{9a280ac0-c8e0-11d1-84e2-00c04fb998a2}', 11)
+RECV_SOCKET_UDPV4 = ('{bf3a50c5-a9c9-4988-a005-2df0b7c80f80}', 11)
 
 
 def kname_to_tuple(name):
