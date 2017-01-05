16cf2296bea6b3e2d245b84951a813913cf7d8e6
rabbitstack <bhnedo@hotmail.com>
2017-01-05 22:59:00 +0100

    using `panic` function on unrecoverable errors. emitting kernel events to defined output sinks. several refactoring

--- fibratus/entrypoint.py
+++ fibratus/entrypoint.py
@@ -14,15 +14,22 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from kstreamc import KEventStreamCollector
-from logbook import Logger, FileHandler
-
 import atexit
 import os
 import sys
+from datetime import datetime
+from datetime import timedelta
+
+from kstreamc import KEventStreamCollector
+
+from fibratus.output.console import ConsoleOutput
+from fibratus.output.elasticsearch import ElasticsearchOutput
+from fibratus.output.smtp import SmtpOutput
+from logbook import Logger, FileHandler, StreamHandler
 from multiprocess import Queue
 
 import fibratus.apidefs.etw as etw
+from fibratus.common import DotD as ddict, panic
 from fibratus.config import YamlConfig
 from fibratus.context_switch import ContextSwitchRegistry
 from fibratus.controller import KTraceController, KTraceProps
@@ -31,12 +38,9 @@ from fibratus.fs import FsIO
 from fibratus.handle import HandleRepository
 from fibratus.kevent import KEvent
 from fibratus.kevent_types import *
-from fibratus.common import DotD as ddict, IO
-from fibratus.output.adapter.amqp import AmqpAdapter
-from fibratus.output.adapter.elasticsearch import ElasticsearchAdapter
-from fibratus.output.adapter.smtp import SmtpAdapter
+from fibratus.tcpip.tcpip import TcpIpParser
+from fibratus.output.amqp import AmqpOutput
 from fibratus.registry import HiveParser
-from fibratus.tcpip import TcpIpParser
 from fibratus.thread import ThreadRegistry
 
 
@@ -52,17 +56,19 @@ class Fibratus(object):
     """
     def __init__(self, filament, **kwargs):
 
+        self._start = datetime.now()
         try:
             log_path = os.path.join(os.path.expanduser('~'), '.fibratus', 'fibratus.log')
             FileHandler(log_path, mode='w+').push_application()
+            StreamHandler(sys.stdout).push_application()
         except PermissionError:
-            IO.write_console("ERROR - Unable to open log file for writing due to permission error")
-            sys.exit(0)
+            panic("ERROR - Unable to open log file for writing due to permission error")
+
         self.logger = Logger(Fibratus.__name__)
 
         self._config = YamlConfig()
 
-        self.logger.info('Starting fibratus...')
+        self.logger.info('Starting...')
 
         enable_cswitch = kwargs.pop('cswitch', False)
 
@@ -84,24 +90,26 @@ class Fibratus(object):
             self.handle_repository.free_buffers()
         self.thread_registry = ThreadRegistry(self.handle_repository, self._handles)
 
-        self.kevt_streamc = KEventStreamCollector(etw.KERNEL_LOGGER_NAME.encode(), self.thread_registry)
-        excluded_procs = self._config.excluded_procs
-        if len(excluded_procs) > 0:
-            self.logger.info("Excluding processes %s" % excluded_procs)
-        self.kevt_streamc.set_excluded_procs(excluded_procs)
+        self.kevt_streamc = KEventStreamCollector(etw.KERNEL_LOGGER_NAME.encode())
+        image_skips = self._config.image_skips
+        if len(image_skips) > 0:
+            self.logger.info("Adding skips for images %s" % image_skips)
+            for skip in image_skips:
+                self.kevt_streamc.add_skip(skip)
 
         self.kevent = KEvent(self.thread_registry)
         self.keventq = Queue()
 
-        self._adapter_classes = dict(smtp=SmtpAdapter,
-                                     amqp=AmqpAdapter,
-                                     elasticsearch=ElasticsearchAdapter)
-        self._output_adapters = self._construct_adapters()
+        self._output_classes = dict(console=ConsoleOutput,
+                                    amqp=AmqpOutput,
+                                    smtp=SmtpOutput,
+                                    elasticsearch=ElasticsearchOutput)
+        self._outputs = self._construct_outputs()
 
         if filament:
             filament.keventq = self.keventq
             filament.logger = log_path
-            filament.setup_adapters(self._output_adapters)
+            filament.setup_adapters(self._outputs)
         self._filament = filament
 
         self.fsio = FsIO(self.kevent, self._handles)
@@ -126,7 +134,8 @@ class Fibratus(object):
 
         def on_kstream_open():
             if self._filament is None:
-                IO.write_console('Done!                               ')
+                delta = datetime.now() - self._start
+                self.logger.info('Started in %s' % str(timedelta(seconds=delta.seconds)))
         self.kevt_streamc.set_kstream_open_callback(on_kstream_open)
         self._open_kstream()
 
@@ -138,80 +147,85 @@ class Fibratus(object):
         except KeyboardInterrupt:
             self.stop_ktrace()
 
-    def _construct_adapters(self):
-        """Instantiates the adapter classes.
+    def _construct_outputs(self):
+        """Instantiates output classes.
 
         Builds the dictionary with instances
-        of the output adapter classes.
+        of the output classes.
         """
-        adapters = {}
-        output_adapters = self._config.output_adapters
-        if output_adapters:
-            for output_adapter in output_adapters:
-                name = next(iter(list(output_adapter.keys())), None)
-                if name and \
-                        name in self._adapter_classes.keys():
-                    # get the adapter configuration
-                    # and instantiate its class
-                    adapter_class = self._adapter_classes[name]
-                    adapter_config = output_adapter[name]
-                    adapters[name] = adapter_class(**adapter_config)
-        return adapters
+        outputs = {}
+        output_configs = self._config.outputs
+        for output in output_configs:
+            name = next(iter(list(output.keys())), None)
+            if name and \
+                    name in self._output_classes.keys():
+                # get the output configuration
+                # and instantiate its class
+                self.logger.info("Initializing %s output" % name)
+                output_class = self._output_classes[name]
+                output_config = output[name]
+                outputs[name] = output_class(**output_config)
+        return outputs
 
     def stop_ktrace(self):
-        IO.write_console('Stopping fibratus...')
+        self.logger.info('Stopping fibratus...')
         if self._filament:
             self._filament.close()
         self.kcontroller.stop_ktrace(self.ktrace_props)
         self.kevt_streamc.close_kstream()
 
     def add_filters(self, kevent_filters, **kwargs):
+        self.kevt_streamc.add_pid_filter(kwargs.pop('pid', None))
         if len(kevent_filters) > 0:
             self.filters_count = len(kevent_filters)
             # include the basic filters
             # that are essential to the
             # rest of kernel events
-            self.kevt_streamc.add_kevent_filter(ENUM_PROCESS)
-            self.kevt_streamc.add_kevent_filter(ENUM_THREAD)
-            self.kevt_streamc.add_kevent_filter(ENUM_IMAGE)
-            self.kevt_streamc.add_kevent_filter(REG_CREATE_KCB)
-            self.kevt_streamc.add_kevent_filter(REG_DELETE_KCB)
+            self.kevt_streamc.add_ktuple_filter(ENUM_PROCESS)
+            self.kevt_streamc.add_ktuple_filter(ENUM_THREAD)
+            self.kevt_streamc.add_ktuple_filter(ENUM_IMAGE)
+            self.kevt_streamc.add_ktuple_filter(REG_CREATE_KCB)
+            self.kevt_streamc.add_ktuple_filter(REG_DELETE_KCB)
 
             # these kevents are necessary for consistent state
             # of the trace. If the user doesn't include them
             # in a filter list, then we do the job but set the
             # kernel event type as not eligible for rendering
             if KEvents.CREATE_PROCESS not in kevent_filters:
-                self.kevt_streamc.add_kevent_filter(CREATE_PROCESS)
+                self.kevt_streamc.add_ktuple_filter(CREATE_PROCESS)
                 self.output_kevents[CREATE_PROCESS] = False
             else:
                 self.output_kevents[CREATE_PROCESS] = True
 
             if KEvents.CREATE_THREAD not in kevent_filters:
-                self.kevt_streamc.add_kevent_filter(CREATE_THREAD)
+                self.kevt_streamc.add_ktuple_filter(CREATE_THREAD)
                 self.output_kevents[CREATE_THREAD] = False
             else:
                 self.output_kevents[CREATE_THREAD] = True
 
-            if KEvents.CREATE_FILE not in kevent_filters:
-                self.kevt_streamc.add_kevent_filter(CREATE_FILE)
-                self.output_kevents[CREATE_FILE] = False
+            if KEvents.TERMINATE_PROCESS not in kevent_filters:
+                self.kevt_streamc.add_ktuple_filter(TERMINATE_PROCESS)
+                self.output_kevents[TERMINATE_PROCESS] = False
             else:
-                self.output_kevents[CREATE_FILE] = True
+                self.output_kevents[TERMINATE_PROCESS] = True
+
+            if KEvents.TERMINATE_THREAD not in kevent_filters:
+                self.kevt_streamc.add_ktuple_filter(TERMINATE_THREAD)
+                self.output_kevents[TERMINATE_THREAD] = False
+            else:
+                self.output_kevents[TERMINATE_THREAD] = True
 
             for kevent_filter in kevent_filters:
                 ktuple = kname_to_tuple(kevent_filter)
                 if isinstance(ktuple, list):
                     for kt in ktuple:
-                        self.kevt_streamc.add_kevent_filter(kt)
+                        self.kevt_streamc.add_ktuple_filter(kt)
                         if kt not in self.output_kevents:
                             self.output_kevents[kt] = True
                 else:
-                    self.kevt_streamc.add_kevent_filter(ktuple)
+                    self.kevt_streamc.add_ktuple_filter(ktuple)
                     if ktuple not in self.output_kevents:
                         self.output_kevents[ktuple] = True
-
-        self.kevt_streamc.add_pid_filter(kwargs.pop('pid', None))
 
     def _on_next_kevent(self, ktype, cpuid, ts, kparams):
         """Callback which fires when new kernel event arrives.
@@ -249,12 +263,12 @@ class Fibratus(object):
                 self.thread_registry.init_thread_kevent(self.kevent,
                                                         ktype,
                                                         kparams)
-                self._render(ktype)
+                self._aggregate(ktype)
         elif ktype in [TERMINATE_PROCESS, TERMINATE_THREAD]:
             self.thread_registry.init_thread_kevent(self.kevent,
                                                     ktype,
                                                     kparams)
-            self._render(ktype)
+            self._aggregate(ktype)
             self.thread_registry.remove_thread(ktype, kparams)
 
         # file system/disk kernel events
@@ -264,18 +278,18 @@ class Fibratus(object):
                        READ_FILE,
                        WRITE_FILE]:
             self.fsio.parse_fsio(ktype, kparams)
-            self._render(ktype)
+            self._aggregate(ktype)
 
         # dll kernel events
         elif ktype in [LOAD_IMAGE, ENUM_IMAGE]:
             self.dll_repository.register_dll(kparams)
             if ktype == LOAD_IMAGE:
-                self._render(ktype)
+                self._aggregate(ktype)
         elif ktype == UNLOAD_IMAGE:
             self.dll_repository.unregister_dll(kparams)
-            self._render(ktype)
-
-        # registry kernel events
+            self._aggregate(ktype)
+        #
+        # # registry kernel events
         elif ktype == REG_CREATE_KCB:
             self.hive_parser.add_kcb(kparams)
         elif ktype == REG_DELETE_KCB:
@@ -289,7 +303,7 @@ class Fibratus(object):
                        REG_DELETE_VALUE,
                        REG_QUERY_VALUE]:
             self.hive_parser.parse_hive(ktype, kparams)
-            self._render(ktype)
+            self._aggregate(ktype)
 
         # network kernel events
         elif ktype in [SEND_SOCKET_TCPV4,
@@ -301,12 +315,12 @@ class Fibratus(object):
                        DISCONNECT_SOCKET_TCPV4,
                        RECONNECT_SOCKET_TCPV4]:
             self.tcpip_parser.parse_tcpip(ktype, kparams)
-            self._render(ktype)
+            self._aggregate(ktype)
 
         # context switch events
         elif ktype == CONTEXT_SWITCH:
             self.context_switch_registry.next_cswitch(cpuid, ts, kparams)
-            self._render(ktype)
+            self._aggregate(ktype)
 
         if self._filament:
             # put the event on the queue
@@ -335,10 +349,10 @@ class Fibratus(object):
                                             comm=thread.comm,
                                             pid=thread.pid,
                                             ppid=thread.ppid))
-                    self._filament.keventq.put(kevt)
+                    self.keventq.put(kevt)
 
-    def _render(self, ktype):
-        """Renders the kevent to the standard output stream.
+    def _aggregate(self, ktype):
+        """Aggregates the kernel event to the output sink.
 
         Parameters
         ----------
@@ -349,6 +363,24 @@ class Fibratus(object):
         if not self._filament:
             if ktype in self.output_kevents:
                 if self.output_kevents[ktype]:
-                    self.kevent.render()
+                    self.kevent.inc_kid()
+                    self._emit()
             elif self.filters_count == 0:
-                self.kevent.render()
+                self.kevent.inc_kid()
+                self._emit()
+
+    def _emit(self):
+        for _, output in self._outputs.items():
+            if isinstance(output, ConsoleOutput):
+                output.emit(self.kevent)
+            else:
+                pid, proc = self.kevent.get_thread()
+                body = {'id': self.kevent.kid,
+                        'timestamp': self.kevent.ts.strftime('%Y-%m-%d %H:%M:%S.%f'),
+                        'cpuid': self.kevent.cpuid,
+                        'proc': proc,
+                        'pid': pid,
+                        'name': self.kevent.name,
+                        'category': self.kevent.category,
+                        'params': self.kevent.params}
+                output.emit(body)
