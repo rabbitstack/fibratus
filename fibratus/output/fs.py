26e500564ea06ab9361c5a90302650123cf84568
rabbitstack <bhnedo@hotmail.com>
2017-04-15 19:48:26 +0200

    file system output closes #15

---
+++ fibratus/output/fs.py
@@ -0,0 +1,55 @@
+# Copyright 2017 by Nedim Sabic (RabbitStack)
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from fibratus.output.base import Output
+import io
+import os
+import time
+import json
+
+
+class FsOutput(Output):
+    """File system output.
+
+    Implementation of the output which writes the stream of
+    kernel events to a file.
+    """
+
+    def __init__(self, **kwargs):
+        super(Output, self).__init__()
+        self._path = kwargs.pop('path', None)
+        self._fmt = kwargs.pop('format', 'json')
+        self._mode = kwargs.pop('mode', 'a')
+
+        filename = os.path.join(self._path,
+                                '%s.fibra' % time.strftime('%x')
+                                .replace('/', '-'))
+        self.stream = io.open(filename, self._mode)
+
+    def emit(self, body, **kwargs):
+        if 'json' in self._fmt:
+            self.stream.write(json.dumps(body) + '\n')
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def format(self):
+        return self._fmt
+
+    @property
+    def mode(self):
+        return self._mode
---
+++ tests/unit/output/fs.py
@@ -0,0 +1,45 @@
+# Copyright 2017 by Nedim Sabic (RabbitStack)
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from io import TextIOBase
+
+from fibratus.output.fs import FsOutput
+from unittest.mock import patch, Mock
+import time
+import os
+import json
+
+
+class TestFsOutput(object):
+
+    def test_init(self):
+        with patch('io.open', return_value=Mock(spec_set=TextIOBase)) as stream_mock:
+            fs_output = FsOutput(path='D:\\', mode='a', format='json')
+            filename = os.path.join(fs_output.path, '%s.fibra' % time.strftime('%x').replace('/', '-'))
+            stream_mock.assert_called_with(filename, 'a')
+            assert 'D:\\' in fs_output.path
+            assert 'a' in fs_output.mode
+            assert 'json' in fs_output.format
+
+    def test_emit(self):
+        with patch('io.open', return_value=Mock(spec_set=TextIOBase)):
+            fs_output = FsOutput(path='D:\\', mode='a', format='json')
+            body = {'kevent_type': 'CreateProcess'}
+            fs_output.emit(body)
+            fs_output.stream.write.assert_called_with(json.dumps(body) + '\n')
+
+    def test_write(self):
+        fs_output = FsOutput(path='D:\\', mode='a', format='json')
+        body = {'kevent_type': 'CreateProcess'}
+        fs_output.emit(body)
