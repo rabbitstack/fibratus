1d9ff66d8d73f203a27e1c321e6c279c5f87021d
Nedim Šabić² <rabbitstack7@gmail.com>
2021-04-25 19:26:04 +0200

    fix rename command

--- make.bat
+++ make.bat
@@ -116,9 +116,9 @@ rm %RELEASE_DIR%\python.zip
 :: is located to advise Windows on the DLL search path strategy.
 move %RELEASE_DIR%\python\*.dll %RELEASE_DIR%\bin
 
-:: Rename libcrypto to libcrypto-1_1-x64.dll. The latter is the DLL name that linker
-:: encounters when using the C compiler msys64.
-ren "%RELEASE_DIR%\bin\libcrypto-1_1.dll" "%RELEASE_DIR%\bin\libcrypto-1_1-x64.dll"
+:: Rename libcrypto-1_1.dll to libcrypto-1_1-x64.dll. Strangely, the latter
+:: is the DLL name required by linker.
+ren "%RELEASE_DIR%\bin\libcrypto-1_1.dll" "libcrypto-1_1-x64.dll"
 
 echo "Building MSI package..."
 heat dir %RELEASE_DIR%\ -cg Fibratus -dr INSTALLDIR -suid -gg -sfrag -srd -var var.FibratusDir -out build/msi/components.wxs || exit /b
