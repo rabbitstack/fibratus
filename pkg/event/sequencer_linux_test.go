856660d85b1dbf42419c09df7c654edec68f2f6b
Mostafa Moradian <mostafa@tigerdata.com>
2026-09-14 13:48:36 +0200

    test(event): Cover the Linux sequencer

---
+++ pkg/event/sequencer_linux_test.go
@@ -0,0 +1,43 @@
+//go:build linux
+
+/*
+ * Copyright 2026 by Mostafa Moradian
+ * https://www.fibratus.io
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package event
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSequencer(t *testing.T) {
+	sequencer := NewSequencer()
+	defer sequencer.Close()
+
+	assert.Equal(t, uint64(0), sequencer.Get())
+	for i := 0; i < 10; i++ {
+		sequencer.Increment()
+	}
+	assert.Equal(t, uint64(10), sequencer.Get())
+
+	require.NoError(t, sequencer.Reset())
+	assert.Equal(t, uint64(0), sequencer.Get())
+	require.NoError(t, sequencer.Shutdown())
+}
